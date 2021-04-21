af050a1a6aafd5c1dbdfb8699809b30819b1f6df
KubaM-97 <jakubm16@interia.pl>
2021-04-21 16:03:17 +0200

    calculatefill fixed

--- src/assets/mixins/rating.js
+++ src/assets/mixins/rating.js
@@ -5,61 +5,127 @@ import { computed } from 'vue'
 export default function mixinRating(){
   
   const store = useStore();
+  const translations = computed(()=>store.state.translations);
+
+    function getVotes_count_container(data){
+
+      const vote_count = data.votes_count.toString();
+      const last_char = vote_count.slice(-1);
+      
+      if( vote_count !== 1 && vote_count < 11 || vote_count > 14 ){
+
+          switch(last_char){  
+
+              case '2':
+              case '3':
+              case '4':
+                return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes')} )`
+
+              default:
+                return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes2')} )`
+
+          }
+
+      }
+      else{
+
+          switch(last_char){  
+
+              case '1':
+                return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes')} )`
+
+              case '12':
+              case '13':
+              case '14':
+                return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes2')})`
+
+              default:
+                return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes2')} )`
+
+          }
+
+      }
+
+    }
+
+    function getVoteSuffix(vote_suffix){
+      return typeof translations.value[vote_suffix] !== 'undefined' ? translations.value[vote_suffix] : 'votes';
+    }
 
     function ratingClick(e){
-      const translations = computed(()=>store.state.translations).value;
-      // let translations = $('.translations').val(JSON.stringify(translations.value));
+      const elem = $(this);
+      const fill = calculateFill(e, $(this).parents('.rating'));
+      const rate = Math.ceil(fill / 100 * 5);
+      setTimeout(function() {
+            $.ajax({
+                url: 'https://widgets.aff44.com/vote?save_rate=' + elem.parents('.a44-offer').attr('data-id') + '&rate=' + rate,
+                dataType: 'jsonp',
+            }).done(function(data) { 
+              // console.log(data)
+                if (data.status === 'success') {
 
-        const elem = $(this);
-        console.log(elem)
-            const fill = calculateFill(e, $(this).parents('.rating'));
-            const rate = Math.ceil(fill / 100 * 5);
-            setTimeout(function() {
-                $.ajax({
-                    url: 'https://widgets.aff44.com/vote?save_rate=' + elem.parents('.a44-offer').attr('data-id') + '&rate=' + rate,
-                    dataType: 'jsonp',
-                }).done(function(data) {
-                    if (data.status === 'success') {
-                        const new_rate = Number(data.new_rate);
-                        elem.parents('.a44-offer').find('.offer-rate').html(new_rate.toFixed(1));
-                        const vote_count = data.votes_count.toString();
-                        const last_char = vote_count.slice(-1);
-                        elem.parents('.a44-offer').find('.votes-count').html('(<b>' + data.votes_count + '</b> ' + (last_char == '1' ? (typeof translations['vote'] !== 'undefined' ? translations['vote'] : 'vote') : (['2', '3', '4'].includes(last_char) ? (typeof translations['votes'] !== 'undefined' ? translations['votes'] : 'votes') : (typeof translations['votes2'] !== 'undefined' ? translations['votes2'] : 'głosów'))) + ')');
-                        elem.parents('.a44-offer').find('.rate').css('width', (new_rate.toFixed(1) / 5 * 100) + '%')
-                    }
-                });
-            }, Math.random() * 300);
+                    const new_rate = Number(data.new_rate);
+
+                    const offer_rate_container = elem.parents('.a44-offer').find('.offer-rate');
+                    offer_rate_container.html(new_rate.toFixed(1));
+
+                    const rate = elem.parents('.a44-offer').find('.rate')
+                    rate.css('width', (new_rate.toFixed(1) / 5 * 100) + '%')
+
+                    const votes_count_container = elem.parents('.a44-offer').find('.votes-count');
+                    votes_count_container.html(getVotes_count_container(data))   
+              
+                }
+            });
+      }, Math.random() * 300);
     }
 
     function ratingHover(e){
-      const fill = calculateFill(e, e.target);
-      $(this).find('.rate').css('width', round(fill, 20, 0) + '%');
+      // GOOD
+      const fill = calculateFill(e);
+      e.target.style.width = round(fill, 20, 0) + '%'
+
+      // BAD
+      // $(this).find('.rate').css('width', round(fill, 20, 0) + '%');
     }
 
     function ratingLeave(){
-        $(this).find('.rate').css('width', $(this).parents('.a44-offer').find('.offer-rate').html() / 5 * 100 + '%'); 
+      
+      // e.target.CHILD(".RATE").style.width = round(fill, 20, 0) + '%'
+      // e.target.CHILD(".RATE").style.width = 
+      // np.4,8 / 5 *100
+      //    $(this).parents('.a44-offer').find('.offer-rate').html()           / 5 * 100 + '%'; 
+      //   $(this).find('.rate').css('width', $(this).parents('.a44-offer').find('.offer-rate').html() / 5 * 100 + '%'); 
     }
 
-    function calculateFill(e, container) {
-      var startCoord = container.offset().left;
-      var endCoord = container.offset().left + container.width();
-      var cursorPosX = e.pageX;
-      var relativeWidth = endCoord - startCoord;
-      var relativeCursorPosX = cursorPosX - startCoord;
-      var percentFilled = relativeCursorPosX / relativeWidth * 100;
+    function calculateFill(e) {
+      const container = e.target;
+      const cursorPosX = e.pageX;
+      const startCoord = container.getBoundingClientRect().left
+      const width = container.getBoundingClientRect().width
+      const relativeCursorPosX = cursorPosX - startCoord;
+      const percentFilled = relativeCursorPosX / width * 100;
       return percentFilled;
-  }
+    }
 
     function round(number, increment, offset) {
       return Math.ceil((number - offset) / increment) * increment + offset;
     }
-
+    function getRateWidth(rate, votes){
+      console.log(rate, votes)
+      //4.7 / 19
+      const totalOfferRate =  rate * votes
+      const totalPossibleRate =  5 * votes
+      const percentFilled = totalOfferRate / totalPossibleRate * 100
+      return percentFilled
+    }
     return{
       ratingClick,
       ratingHover,
       ratingLeave,
       calculateFill,
-      round
+      round,
+      getRateWidth
     }
 
 }
--- src/components/specificOffers.vue
+++ src/components/specificOffers.vue
@@ -29,8 +29,8 @@
                     <div class="stars">
                       <div class="rank-rate">
                         <span>
-                          <div class="rating">
-                            <div class="rate" :style="{width: getRateInitWidth( offer.rate , offer.votes_count ) + '%'}"></div>
+                          <div class="rating" @mousemove="ratingHover" @mouseleave="getRateWidth( offer.rate , offer.votes_count )">
+                            <div class="rate" :style="{width: getRateWidth( offer.rate , offer.votes_count ) + '%'}"></div>
                           </div>
                         </span>
                       </div>
@@ -103,10 +103,9 @@
 
 <script>
 
-import { computed, onMounted } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
 import mixinRating from "../assets/mixins/rating.js"
-import $ from 'jquery'
 
 export default {
   
@@ -122,108 +121,15 @@ export default {
         const rrso = computed(()=>store.state.rrso);
         const arr = computed(()=>store.state.arr);
 
-        const { calculateFill, round } = mixinRating();
-
-        function getRateInitWidth(rate, votes){
-          const totalOfferRate =  rate * votes
-          const totalPossibleRate =  5 * votes
-          const percentFilled = totalOfferRate / totalPossibleRate * 100
-          return percentFilled
-        }
-        
-        onMounted(()=>{
-          $('.rating').on('mousemove', function(e) {
-              var fill = calculateFill(e, $(this));
-              $(this).find('.rate').css('width', round(fill, 20, 0) + '%');
-          });
-          $('.rating').on('mouseleave', function() {
-              $(this).find('.rate').css('width', $(this).parents('.a44-offer').find('.offer-rate').html() / 5 * 100 + '%');
-          });
-          
-          
-          $('.rate').on('click', function(e) {
-            
-
-            const elem = $(this);
-            const fill = calculateFill(e, $(this).parents('.rating'));
-            const rate = Math.ceil(fill / 100 * 5);
-            setTimeout(function() {
-                  $.ajax({
-                      url: 'https://widgets.aff44.com/vote?save_rate=' + elem.parents('.a44-offer').attr('data-id') + '&rate=' + rate,
-                      dataType: 'jsonp',
-                  }).done(function(data) { 
-                    // console.log(data)
-                      if (data.status === 'success') {
-
-                          const new_rate = Number(data.new_rate);
-
-                          const offer_rate_container = elem.parents('.a44-offer').find('.offer-rate');
-                          offer_rate_container.html(new_rate.toFixed(1));
-
-                          const rate = elem.parents('.a44-offer').find('.rate')
-                          rate.css('width', (new_rate.toFixed(1) / 5 * 100) + '%')
-
-                          const votes_count_container = elem.parents('.a44-offer').find('.votes-count');
-                          votes_count_container.html(getVotes_count_container(data))   
-                    
-                      }
-                  });
-            }, Math.random() * 300);
-
-        })
-
-          function getVotes_count_container(data){
-
-            const vote_count = data.votes_count.toString();
-            const last_char = vote_count.slice(-1);
-            
-            if( vote_count !== 1 && vote_count < 11 || vote_count > 14 ){
-
-                switch(last_char){  
-
-                    case '2':
-                    case '3':
-                    case '4':
-                      return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes')} )`
-
-                    default:
-                      return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes2')} )`
-
-                }
-
-            }
-            else{
-
-                switch(last_char){  
-
-                    case '1':
-                      return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes')} )`
-
-                    case '12':
-                    case '13':
-                    case '14':
-                      return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes2')})`
-
-                    default:
-                      return `(<b> ${data.votes_count} </b> ${getVoteSuffix('votes2')} )`
-
-                }
-
-            }
-
-          }
-
-          function getVoteSuffix(vote_suffix){
-            return typeof translations.value[vote_suffix] !== 'undefined' ? translations.value[vote_suffix] : 'votes';
-          }
-
-        })
+        const { ratingHover, getRateWidth } = mixinRating();
         
         return {
-          getRateInitWidth,
+          ratingHover,
+          getRateWidth,
           translations,
           rrso,
-          arr
+          arr,
+          mixinRating
         };
 
     },
